295d8dab4f62490552f2ee279ce143c546b231d6
Kigen Jones <kigenjonesrm@gmail.com>
2025-09-30 15:42:11 -0500

    init commit

---
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class CellPhone {
+    private String model;
+    private String phoneNumber;
+
+    public CellPhone() {
+        model = "";
+        phoneNumber = "";
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
