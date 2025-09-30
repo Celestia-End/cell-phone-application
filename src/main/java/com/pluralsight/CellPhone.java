f4e6cd24c4316b7f06ca2736532519feff73f2e8
Kigen Jones <kigenjonesrm@gmail.com>
2025-09-30 16:45:02 -0500

    init commit

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,19 +1,62 @@
 package com.pluralsight;
 
 public class CellPhone {
-    private String model;
-    private String phoneNumber;
+        private int serialNumber;
+        private String model;
+        private String carrier;
+        private String phoneNumber;
+        private String owner;
 
-    public CellPhone() {
-        model = "";
-        phoneNumber = "";
-    }
+        public CellPhone() {
+            this.serialNumber = 0;
+            this.model = "";
+            this.carrier = "";
+            this.phoneNumber = "";
+            this.owner = "";
+        }
 
-    public String getModel() {
-        return model;
-    }
+        //Getter & Setter for Serial Number
+        public int getSerialNumber() {
+            return serialNumber;
+        }
 
-    public void setModel(String model) {
-        this.model = model;
+        public void setSerialNumber(int serialNumber) {
+            this.serialNumber = serialNumber;
+        }
+
+        //Getter & Setter for Model
+        public String getModel() {
+            return model;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        //Getter & Setter for Carrier
+        public String getCarrier() {
+            return carrier;
+        }
+
+        public void setCarrier(String carrier) {
+            this.carrier = carrier;
+        }
+
+        //Getter & Setter for Phone Number
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        //Getter & Setter for Owner
+        public String getOwner() {
+            return owner;
+        }
+
+        public void setOwner(String owner) {
+            this.owner = owner;
+        }
     }
-}
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++
@@ -1,8 +0,0 @@
-package com.pluralsight;
-
-public class CellPhoneApplication {
-
-    public static void main(String[] args) {
-
-    }
-}
